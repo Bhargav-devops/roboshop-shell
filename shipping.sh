1aa21b6873fb9ddcecbdab73a5f434c8c9423c21
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-23 19:38:37 +0530

    mysql and shipping

--- shipping.sh
+++ shipping.sh
@@ -29,3 +29,52 @@ then
 else
     echo -e "you are root user"
 fi
+
+dnf install maven -y
+
+
+id roboshop &>> $LogFile
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LogFile
+    Validate $? "creating roboshop user "
+else
+    echo -e "user already exist $Y skipping $N"
+fi
+mkdir -p /app &>> $LogFile
+Validate $? "creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+Validate $? "Downloading shipping application"
+
+cd /app
+
+unzip /tmp/shipping.zip &>> $LogFile
+Validate $? "unzip shipping application"
+
+mvn clean package &>> $LogFile
+Validate $? "building the application"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LogFile
+Validate $? "renaming the shipping jar"
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service 
+Validate $? "copying shipping service to etc"
+
+systemctl daemon-reload &>> $LogFile
+Validate $? "daemon Reload"
+
+systemctl enable shipping &>> $LogFile
+Validate $? "enable shipping"
+
+systemctl start shipping &>> $LogFile
+Validate $? "start shipping"
+
+dnf install mysql -y &>> $LogFile
+Validate $? "installing mysql"
+
+mysql -h mysql.bhargavdevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LogFile
+Validate $? "loading shipping data"
+
+systemctl restart shipping &>> $LogFile
+Validate $? "restart shipping"
