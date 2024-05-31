8d4c704b46aa87b052895ac4e24054d01ebf4567
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-05-31 23:07:03 +0530

    automating instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -4,6 +4,9 @@
 
 AMI="ami-0f3c7d07486cad139"
 SG_ID="sg-01d0fa686e2d4cfbc"
+ZoneID="Z0390089TIO0SFJPINO3"
+DomainName="bhargavdevops.online"
+
 Instances=("mongodb" "mysql" "shipping" "catalogue" "cart" "user" "redis" "payment" "rabbitmq" "dispatch" "web")
 for i in "${Instances[@]}"
 do
@@ -14,5 +17,26 @@ do
     else
         InstanceType="t2.micro"
     fi
-aws ec2 run-instances --image-id $AMI --instance-type $InstanceType --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text
+IP_Address=$(aws ec2 run-instances --image-id $AMI --instance-type $InstanceType --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text)
+
+echo " $i = $IP_Address"
+#create route 53 records, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id 1234567890ABC \
+    --change-batch '
+    {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "$i.$DomainName"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "$IP_Address"
+            }]
+        }
+        }]
+    }
+    '
 done
